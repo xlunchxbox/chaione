f114e1e1d596e1a0018a6ea453a9e16c474ac466
Christopher Cruz <chris1cruz@gmail.com>
2013-07-11 15:40:36 -0500

    add comments

--- donkey_kong.rb
+++ donkey_kong.rb
@@ -1,12 +1,15 @@
 def donkey_kong
+  #loop through from 1 to 100
   (1..100).each do |n|
     
+    #Check to see if both divisble by 3 & 5 first, then just 3, then 5, if none
+    #then print the number
     if (n % 3 == 0) && (n % 5 == 0)
       print "#{n} Donkey Kong\n"
-  
+      
     elsif n % 3 == 0
       print "#{n} Donkey \n"
-  
+    
     elsif n % 5 == 0 
       print "#{n} Kong \n"
     else
--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,18 +1,19 @@
-
-
 def roman_numeral_convertor(n)
+  #array of base values and weird values like 4 & 9
   base_numbers =   [1000, 900, 500, 400, 100,90, 50, 40, 10, 9, 5, 4, 1]
+  
+  #array of Roman Numerals
   roman_numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-  roman_numeral = ""
-  i = 0
+  converted_roman_numeral = ""
  
+  #loop through and compare passed number to base_numbers
   base_numbers.length.times do |i|
     while n >= base_numbers[i]
       n = n - base_numbers[i]
-      roman_numeral = roman_numeral + roman_numerals[i]
+      converted_roman_numeral = converted_roman_numeral + roman_numerals[i]
     end
   end
-  roman_numeral
+  converted_roman_numeral
 end
  
  
